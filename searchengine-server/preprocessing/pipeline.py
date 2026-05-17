a9bd1f0057601226bd9fc950032dd1038a66b9e2
Yacine Akli <yacine.akli97@gmail.com>
2026-05-17 07:11:53 +0200

    Fix file paths in pipeline.py script

--- searchengine-server/preprocessing/pipeline.py
+++ searchengine-server/preprocessing/pipeline.py
@@ -14,10 +14,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 output_dir = BASE_DIR / "resources" / "python-processed"
 output_dir.mkdir(parents=True, exist_ok=True)
 
+SCRIPT_DIR = Path(__file__).resolve().parent
 for script in scripts:
     print(f"\nRunning {script}...\n")
+    script_path = SCRIPT_DIR / script
+
     result = subprocess.run(
-        [sys.executable, f"searchengine-server/preprocessing/{script}"],
+        [sys.executable, str(script_path)],
         check=True
     )
 
